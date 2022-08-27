9a902c249471ba8c59cb769e4caa5ed3c826e2c0
Tim Schumacher <timschumi@gmx.de>
2022-08-27 12:34:39 +0200

    Move the Download Mode reboot code to a proper subcommand

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,13 @@ fn cli() -> Command<'static> {
                         .default_value(&"04e8"),
                 ),
         )
-        .subcommand(Command::new("reboot-to-upload").about("Reboot into upload mode"))
+        .subcommand(
+            Command::new("download")
+                .about("Talking to Download Mode")
+                .subcommand_required(true)
+                .arg_required_else_help(true)
+                .subcommand(Command::new("reboot").about("Reboot the device")),
+        )
         .arg(arg!(--device <ID> "The vendor and device ID to communicate with").required(false))
 }
 
@@ -98,46 +104,58 @@ fn main() {
 
     device.setup_interface().unwrap();
 
-    device
-        .write(&[0x4f, 0x44, 0x49, 0x4e], Duration::from_secs(1))
-        .unwrap();
+    match matches.subcommand() {
+        Some(("download", sub_matches)) => {
+            device
+                .write(&[0x4f, 0x44, 0x49, 0x4e], Duration::from_secs(1))
+                .unwrap();
 
-    let mut hello_response = [0u8; 4];
+            let mut hello_response = [0u8; 4];
 
-    device
-        .read(&mut hello_response, Duration::from_secs(1))
-        .unwrap();
+            device
+                .read(&mut hello_response, Duration::from_secs(1))
+                .unwrap();
 
-    assert_eq!(
-        hello_response[0..4],
-        [0x4C, 0x4F, 0x4B, 0x45],
-        "Protocol hello response not as expected: {:?}",
-        hello_response
-    );
+            assert_eq!(
+                hello_response[0..4],
+                [0x4C, 0x4F, 0x4B, 0x45],
+                "Protocol hello response not as expected: {:?}",
+                hello_response
+            );
 
-    device
-        .write_packet(
-            &[0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
-            1024,
-            Duration::from_secs(1),
-        )
-        .unwrap();
+            device
+                .write_packet(
+                    &[0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                    1024,
+                    Duration::from_secs(1),
+                )
+                .unwrap();
 
-    device
-        .read(&mut [0u8; 1024], Duration::from_secs(1))
-        .unwrap();
+            device
+                .read(&mut [0u8; 1024], Duration::from_secs(1))
+                .unwrap();
 
-    device
-        .write_packet(
-            &[0x67, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00],
-            1024,
-            Duration::from_secs(1),
-        )
-        .unwrap();
+            match sub_matches.subcommand() {
+                Some(("reboot", _)) => {
+                    // Does nothing, we will reboot at the end of the session anyways.
+                }
+                _ => unreachable!(),
+            }
 
-    device
-        .read(&mut [0u8; 1024], Duration::from_secs(1))
-        .unwrap();
+            device
+                .write_packet(
+                    &[0x67, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00],
+                    1024,
+                    Duration::from_secs(1),
+                )
+                .unwrap();
+
+            device
+                .read(&mut [0u8; 1024], Duration::from_secs(1))
+                .unwrap();
+        }
+        _ => unreachable!(),
+    }
 
     device.teardown_interface().unwrap();
 }
